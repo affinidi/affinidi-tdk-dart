4e438ea25360a6a10e1a24ec71ff47195b63d437
Yaroslav Smolyaninov <yaroslav.s@affinidi.com>
2025-06-23 15:42:14 +0200

    feat: add integration tests

--- packages/vault_storages/test/mocks/mock_iam_client.dart
+++
@@ -1,8 +0,0 @@
-import 'package:affinidi_tdk_iam_client/affinidi_tdk_iam_client.dart';
-import 'package:mocktail/mocktail.dart';
-
-class MockAuthzApi extends Mock implements AuthzApi {}
-
-class MockAffinidiTdkIamClient extends Mock implements AffinidiTdkIamClient {}
-
-class GrantAccessInputFake extends Fake implements GrantAccessInput {}
