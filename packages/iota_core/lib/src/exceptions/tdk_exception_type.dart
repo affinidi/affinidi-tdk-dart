4b9a111813c7ef12d5ed698a269cd82d8165e055
khoavo1808 <khoa.v@affinidi.com>
2026-05-05 12:01:47 +0800

    chore: update tdk exception type comments
    Revert "chore: update tdk exception type comments"
    
    This reverts commit d55f03c0db50244412c820b208915461c85f7572.
    
    chore: update exception type comment

--- packages/iota_core/lib/src/exceptions/tdk_exception_type.dart
+++ packages/iota_core/lib/src/exceptions/tdk_exception_type.dart
@@ -8,16 +8,16 @@
 /// Each exception type has the following properties:
 /// - [code] - string code that uniquely identifies the exception type.
 enum TdkExceptionType {
-  /// The JWT in the request URI is invalid or has expired.
+  /// Exception thrown when the JWT in the request URI is invalid or has expired.
   invalidOrExpiredJwt(code: 'invalid_or_expired_jwt'),
 
-  /// The `response_mode` in the request is not `direct_post`.
+  /// Exception thrown when the `response_mode` in the request is not `direct_post`.
   invalidResponseMode(code: 'invalid_response_mode'),
 
-  /// The `client_id` field is missing from the request.
+  /// Exception thrown when the `client_id` field is missing from the request.
   missingClientId(code: 'missing_client_id'),
 
-  /// The URI could not be parsed or a required field was missing.
+  /// Exception thrown when the URI could not be parsed or a required field was missing.
   parseFailure(code: 'parse_failure');
 
   const TdkExceptionType({required this.code});
