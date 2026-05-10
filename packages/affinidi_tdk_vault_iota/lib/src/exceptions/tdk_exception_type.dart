9a725e4017c7733a1b382d1ea143782366ff92ff
khoavo1808 <khoa.v@affinidi.com>
2026-05-10 17:45:36 +0800

    chore: remove unused exception

--- packages/affinidi_tdk_vault_iota/lib/src/exceptions/tdk_exception_type.dart
+++ packages/affinidi_tdk_vault_iota/lib/src/exceptions/tdk_exception_type.dart
@@ -1,17 +1,5 @@
 /// Types of exceptions that can occur in the Vault Iota share flow.
 enum TdkExceptionType {
-  /// Exception thrown when the JWT in the request URI is invalid or has expired.
-  invalidOrExpiredJwt('invalid_or_expired_jwt'),
-
-  /// Exception thrown when the `response_mode` in the request is not `direct_post`.
-  invalidResponseMode('invalid_response_mode'),
-
-  /// Exception thrown when the `client_id` field is missing from the request.
-  missingClientId('missing_client_id'),
-
-  /// Exception thrown when the URI could not be parsed or a required field was missing.
-  parseFailure('parse_failure'),
-
   /// Exception thrown when the verifier's client metadata could not be fetched
   /// or parsed.
   verifierMetadataFetchFailed('verifier_metadata_fetch_failed');
