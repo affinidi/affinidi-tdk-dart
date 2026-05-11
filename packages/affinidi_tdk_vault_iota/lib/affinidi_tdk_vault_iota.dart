3b2c2de178307615e675836bdbb5f69d02a07ce2
khoavo1808 <khoa.v@affinidi.com>
2026-05-11 14:25:44 +0800

    chore: refactor rename pd_classifier to pd_classifier_service

--- packages/affinidi_tdk_vault_iota/lib/affinidi_tdk_vault_iota.dart
+++ packages/affinidi_tdk_vault_iota/lib/affinidi_tdk_vault_iota.dart
@@ -11,5 +11,5 @@ export 'src/models/pd_requirements.dart';
 export 'src/models/request_purpose.dart';
 export 'src/models/submission_requirements.dart';
 export 'src/models/verified_identity_document_info.dart';
-export 'src/services/pd_classifier.dart';
+export 'src/services/pd_classifier_service.dart';
 
