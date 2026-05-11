6c2c63e3e3cbfe01588d728c84501c7294ea9cc2
abrishs <abrish.s@affinidi.com>
2026-05-11 16:22:29 +0200

    fix: folder creation to use nodeId (#103)

--- packages/vault_data_manager/test/vfs_shared_storage_test.dart
+++ packages/vault_data_manager/test/vfs_shared_storage_test.dart
@@ -43,7 +43,7 @@ void main() {
         folderName: any(named: 'folderName'),
         parentNodeId: any(named: 'parentNodeId'),
       ),
-    ).thenAnswer((_) async {});
+    ).thenAnswer((_) async => 'created-folder-id');
     when(
       () => mockDataManagerService.deleteFile(any()),
     ).thenAnswer((_) async {});
@@ -154,7 +154,7 @@ void main() {
             folderName: any(named: 'folderName'),
             parentNodeId: any(named: 'parentNodeId'),
           ),
-        ).thenAnswer((_) async {});
+        ).thenAnswer((_) async => 'created-folder-id');
 
         final folderNode = Node(
           nodeId: '1',
@@ -174,18 +174,11 @@ void main() {
         );
 
         when(
-          () => mockDataManagerService.getChildNodes(
-            nodeId: NodeFixtures.testParentId,
-            limit: any(named: 'limit'),
-            exclusiveStartItemId: any(named: 'exclusiveStartItemId'),
+          () => mockDataManagerService.getNodeInfo(
+            'created-folder-id',
             cancelToken: any(named: 'cancelToken'),
           ),
-        ).thenAnswer(
-          (_) async => PaginatedList<Node>(
-            items: [folderNode],
-            lastEvaluatedItemId: null,
-          ),
-        );
+        ).thenAnswer((_) async => folderNode);
 
         await storage.createFolder(
           folderName: FileFixtures.testFolderName,
