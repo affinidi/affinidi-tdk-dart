da0f9745a06a4d20d5fdfbd2696617ae25541364
Marco Attanasio <marco.a@affinidi.com>
2026-05-20 09:42:00 +0200

    fix: solve static analysis issue

--- packages/claim_verifiable_credential/lib/oid4vci_claim_verifiable_credential.dart
+++ packages/claim_verifiable_credential/lib/oid4vci_claim_verifiable_credential.dart
@@ -2,4 +2,6 @@
 ///
 /// Prefer importing
 /// `package:affinidi_tdk_claim_verifiable_credential/affinidi_tdk_claim_verifiable_credential.dart`.
+library;
+
 export 'affinidi_tdk_claim_verifiable_credential.dart';
