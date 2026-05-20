dcf5dc49c20f1ddc885f6a437697986761252847
Khoa Vo <khoa.v@affinidi.com>
2026-05-20 10:34:46 +0800

    feat: parse and validate an OID4VP share request URI (#100)
    * chore: implement iota OID4VP models and error types to iota_core
    
    * chore: implement IotaService for OID4VP URI parsing
    
    * chore: add unit tests for IotaService
    
    * chore: format files
    
    chore: add comment to iota core class
    
    * chore: update tdk exception type comments
    
    Revert "chore: update tdk exception type comments"
    
    This reverts commit d55f03c0db50244412c820b208915461c85f7572.
    
    chore: update exception type comment
    
    * chore: fix validation order and error codes in IotaService
    
    * chore: update tests
    
    chore: format file
    
    * chore: add cryptography test
    
    * chore: format file
    
    * chore: remove publish to none in iota core package
    
    * chore: exclude integration test in pipeline
    
    chore: format file
    
    * chore: update comment to correct exception case
    
    * chore: remove change in iota core package
    
    * chore: move share flow to vault package
    
    * chore: update models, validation service
    
    * chore: add CHANGE LOG
    
    * chore: format
    
    * chore: move files to affinidi_vault_tdk_iota package
    
    * chore: update change log + separate concers to extensions files
    
    * chore: revert changes in iota-core and vault package
    
    * fix: fix pipeline failed
    
    * Revert "fix: fix pipeline failed"
    
    This reverts commit c1cf3d3c6ec3a49368c6ebe6809aabbc468c92e2.
    
    * chore: update types and exports
    
    * chore: refactor fixture to separate file
    
    * chore: format file
    
    * chore: throw without stacktrace and create private method to throw exception in the service
    
    * chore: separate exception catch + add tests
    
    * chore: format file

--- packages/affinidi_tdk_vault_iota/CHANGELOG.md
+++ packages/affinidi_tdk_vault_iota/CHANGELOG.md
@@ -2,4 +2,7 @@
 
 ## 1.0.0-dev
 
-- Initial version.
+- Initial release with OID4VP share flow support.
+- Parse and validate Iota OID4VP request URIs.
+- Verify JWT signatures and expiry.
+- Extract presentation definition and purpose metadata.
--- packages/affinidi_tdk_vault_iota/lib/src/exceptions/tdk_exception_type.dart
+++ packages/affinidi_tdk_vault_iota/lib/src/exceptions/tdk_exception_type.dart
@@ -4,13 +4,38 @@ enum TdkExceptionType {
   /// or parsed.
   failedToFetchVerifierMetadata('failed_to_fetch_verifier_metadata'),
 
-  /// Exception thrown when an empty [clientId] is passed to the verifier
+  /// Exception thrown when an empty `clientId` is passed to the verifier
   /// metadata service.
-  invalidClientId('invalid_client_id');
+  invalidClientId('invalid_client_id'),
+
+  /// Exception thrown when the JWT in the request URI is invalid or has expired.
+  invalidOrExpiredJwt('invalid_or_expired_jwt'),
+
+  /// Exception thrown when the `response_mode` in the request is not `direct_post`.
+  invalidResponseMode('invalid_response_mode'),
+
+  /// Exception thrown when the `client_id` field is missing from the request.
+  missingClientId('missing_client_id'),
+
+  /// Exception thrown when the `client_id_scheme` in the request is not `did`.
+  invalidClientIdScheme('invalid_client_id_scheme'),
+
+  /// Exception thrown when the JWT `aud` claim does not match the wallet DID.
+  invalidAudience('invalid_audience'),
+
+  /// Exception thrown when the URI could not be parsed or a required field was missing.
+  parseFailure('parse_failure'),
+
+  /// Thrown when a Presentation Definition is structurally invalid.
+  invalidPresentationDefinition('invalid_presentation_definition'),
+
+  /// Thrown when a single IDV input descriptor requests more than two VC types.
+  unsupportedMultipleIdvTypes('unsupported_multiple_idv_types'),
+
+  /// Thrown when submitting the VP to the verifier callback fails.
+  submissionFailed('submission_failed');
 
   /// Creates a new instance of [TdkExceptionType].
-  ///
-  /// [code] - The error code associated with this exception type.
   const TdkExceptionType(this.code);
 
   /// The error code associated with this exception type.
