c50631d078bccb218e93ddef6d1ab59715fa328c
khoavo1808 <khoa.v@affinidi.com>
2026-06-10 12:28:31 +0800

    chore: add test for share requirement matcher

---
+++ packages/affinidi_tdk_vault_iota/test/mocks/mock_dcql_share_requirements_matcher.dart
@@ -0,0 +1,6 @@
+import 'package:affinidi_tdk_vault_iota/src/services/dcql_share_requirements_matcher_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+/// A mocktail mock for [DcqlShareRequirementsMatcher].
+class MockDcqlShareRequirementsMatcher extends Mock
+    implements DcqlShareRequirementsMatcher {}
---
+++ packages/affinidi_tdk_vault_iota/test/mocks/mock_pd_classifier.dart
@@ -0,0 +1,5 @@
+import 'package:affinidi_tdk_vault_iota/affinidi_tdk_vault_iota.dart';
+import 'package:mocktail/mocktail.dart';
+
+/// A mocktail mock for [PDClassifier].
+class MockPDClassifier extends Mock implements PDClassifier {}
