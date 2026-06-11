857d121aee0c1048ed45c585192d9a776050f3bf
Affinidi Pte. Ltd. <pub.dev@affinidi.com>
2026-06-11 22:28:57 +0000

    feat: update all clients

--- clients/wallets_client/lib/src/model/create_wallet_key_input.g.dart
+++ clients/wallets_client/lib/src/model/create_wallet_key_input.g.dart
@@ -6,6 +6,41 @@ part of 'create_wallet_key_input.dart';
 // BuiltValueGenerator
 // **************************************************************************
 
+const CreateWalletKeyInputAlgorithmEnum
+_$createWalletKeyInputAlgorithmEnum_secp256k1 =
+    const CreateWalletKeyInputAlgorithmEnum._('secp256k1');
+const CreateWalletKeyInputAlgorithmEnum
+_$createWalletKeyInputAlgorithmEnum_ed25519 =
+    const CreateWalletKeyInputAlgorithmEnum._('ed25519');
+const CreateWalletKeyInputAlgorithmEnum
+_$createWalletKeyInputAlgorithmEnum_p256 =
+    const CreateWalletKeyInputAlgorithmEnum._('p256');
+
+CreateWalletKeyInputAlgorithmEnum _$createWalletKeyInputAlgorithmEnumValueOf(
+  String name,
+) {
+  switch (name) {
+    case 'secp256k1':
+      return _$createWalletKeyInputAlgorithmEnum_secp256k1;
+    case 'ed25519':
+      return _$createWalletKeyInputAlgorithmEnum_ed25519;
+    case 'p256':
+      return _$createWalletKeyInputAlgorithmEnum_p256;
+    default:
+      throw ArgumentError(name);
+  }
+}
+
+final BuiltSet<CreateWalletKeyInputAlgorithmEnum>
+_$createWalletKeyInputAlgorithmEnumValues =
+    BuiltSet<CreateWalletKeyInputAlgorithmEnum>(
+      const <CreateWalletKeyInputAlgorithmEnum>[
+        _$createWalletKeyInputAlgorithmEnum_secp256k1,
+        _$createWalletKeyInputAlgorithmEnum_ed25519,
+        _$createWalletKeyInputAlgorithmEnum_p256,
+      ],
+    );
+
 const CreateWalletKeyInputKeyTypeEnum
 _$createWalletKeyInputKeyTypeEnum_secp256k1 =
     const CreateWalletKeyInputKeyTypeEnum._('secp256k1');
@@ -40,9 +75,47 @@ _$createWalletKeyInputKeyTypeEnumValues =
       ],
     );
 
+Serializer<CreateWalletKeyInputAlgorithmEnum>
+_$createWalletKeyInputAlgorithmEnumSerializer =
+    _$CreateWalletKeyInputAlgorithmEnumSerializer();
 Serializer<CreateWalletKeyInputKeyTypeEnum>
 _$createWalletKeyInputKeyTypeEnumSerializer =
     _$CreateWalletKeyInputKeyTypeEnumSerializer();
+
+class _$CreateWalletKeyInputAlgorithmEnumSerializer
+    implements PrimitiveSerializer<CreateWalletKeyInputAlgorithmEnum> {
+  static const Map<String, Object> _toWire = const <String, Object>{
+    'secp256k1': 'secp256k1',
+    'ed25519': 'ed25519',
+    'p256': 'p256',
+  };
+  static const Map<Object, String> _fromWire = const <Object, String>{
+    'secp256k1': 'secp256k1',
+    'ed25519': 'ed25519',
+    'p256': 'p256',
+  };
+
+  @override
+  final Iterable<Type> types = const <Type>[CreateWalletKeyInputAlgorithmEnum];
+  @override
+  final String wireName = 'CreateWalletKeyInputAlgorithmEnum';
+
+  @override
+  Object serialize(
+    Serializers serializers,
+    CreateWalletKeyInputAlgorithmEnum object, {
+    FullType specifiedType = FullType.unspecified,
+  }) => _toWire[object.name] ?? object.name;
+
+  @override
+  CreateWalletKeyInputAlgorithmEnum deserialize(
+    Serializers serializers,
+    Object serialized, {
+    FullType specifiedType = FullType.unspecified,
+  }) => CreateWalletKeyInputAlgorithmEnum.valueOf(
+    _fromWire[serialized] ?? (serialized is String ? serialized : ''),
+  );
+}
 
 class _$CreateWalletKeyInputKeyTypeEnumSerializer
     implements PrimitiveSerializer<CreateWalletKeyInputKeyTypeEnum> {
@@ -81,7 +154,9 @@ class _$CreateWalletKeyInputKeyTypeEnumSerializer
 
 class _$CreateWalletKeyInput extends CreateWalletKeyInput {
   @override
-  final CreateWalletKeyInputKeyTypeEnum keyType;
+  final CreateWalletKeyInputAlgorithmEnum? algorithm;
+  @override
+  final CreateWalletKeyInputKeyTypeEnum? keyType;
   @override
   final BuiltList<VerificationRelationship> relationships;
 
@@ -89,8 +164,11 @@ class _$CreateWalletKeyInput extends CreateWalletKeyInput {
     void Function(CreateWalletKeyInputBuilder)? updates,
   ]) => (CreateWalletKeyInputBuilder()..update(updates))._build();
 
-  _$CreateWalletKeyInput._({required this.keyType, required this.relationships})
-    : super._();
+  _$CreateWalletKeyInput._({
+    this.algorithm,
+    this.keyType,
+    required this.relationships,
+  }) : super._();
   @override
   CreateWalletKeyInput rebuild(
     void Function(CreateWalletKeyInputBuilder) updates,
@@ -104,6 +182,7 @@ class _$CreateWalletKeyInput extends CreateWalletKeyInput {
   bool operator ==(Object other) {
     if (identical(other, this)) return true;
     return other is CreateWalletKeyInput &&
+        algorithm == other.algorithm &&
         keyType == other.keyType &&
         relationships == other.relationships;
   }
@@ -111,6 +190,7 @@ class _$CreateWalletKeyInput extends CreateWalletKeyInput {
   @override
   int get hashCode {
     var _$hash = 0;
+    _$hash = $jc(_$hash, algorithm.hashCode);
     _$hash = $jc(_$hash, keyType.hashCode);
     _$hash = $jc(_$hash, relationships.hashCode);
     _$hash = $jf(_$hash);
@@ -120,6 +200,7 @@ class _$CreateWalletKeyInput extends CreateWalletKeyInput {
   @override
   String toString() {
     return (newBuiltValueToStringHelper(r'CreateWalletKeyInput')
+          ..add('algorithm', algorithm)
           ..add('keyType', keyType)
           ..add('relationships', relationships))
         .toString();
@@ -129,6 +210,11 @@ class _$CreateWalletKeyInput extends CreateWalletKeyInput {
 class CreateWalletKeyInputBuilder
     implements Builder<CreateWalletKeyInput, CreateWalletKeyInputBuilder> {
   _$CreateWalletKeyInput? _$v;
+
+  CreateWalletKeyInputAlgorithmEnum? _algorithm;
+  CreateWalletKeyInputAlgorithmEnum? get algorithm => _$this._algorithm;
+  set algorithm(CreateWalletKeyInputAlgorithmEnum? algorithm) =>
+      _$this._algorithm = algorithm;
 
   CreateWalletKeyInputKeyTypeEnum? _keyType;
   CreateWalletKeyInputKeyTypeEnum? get keyType => _$this._keyType;
@@ -148,6 +234,7 @@ class CreateWalletKeyInputBuilder
   CreateWalletKeyInputBuilder get _$this {
     final $v = _$v;
     if ($v != null) {
+      _algorithm = $v.algorithm;
       _keyType = $v.keyType;
       _relationships = $v.relationships.toBuilder();
       _$v = null;
@@ -174,11 +261,8 @@ class CreateWalletKeyInputBuilder
       _$result =
           _$v ??
           _$CreateWalletKeyInput._(
-            keyType: BuiltValueNullFieldError.checkNotNull(
-              keyType,
-              r'CreateWalletKeyInput',
-              'keyType',
-            ),
+            algorithm: algorithm,
+            keyType: keyType,
             relationships: relationships.build(),
           );
     } catch (_) {
