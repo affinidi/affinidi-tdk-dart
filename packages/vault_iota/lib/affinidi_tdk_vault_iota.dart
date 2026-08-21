b298fca48cf4e5a73c7875e222bfb77b841c38ee
Marco Attanasio <marco.a@affinidi.com>
2026-08-21 13:51:23 +0200

    chore: minor cleanup

--- packages/vault_iota/lib/affinidi_tdk_vault_iota.dart
+++ packages/vault_iota/lib/affinidi_tdk_vault_iota.dart
@@ -3,6 +3,7 @@
 library;
 
 export 'package:affinidi_tdk_common/affinidi_tdk_common.dart' show TdkException;
+
 export 'src/exceptions/tdk_exception_type.dart';
 export 'src/models/auto_consent_result.dart';
 export 'src/models/claimed_credentials_result.dart';
