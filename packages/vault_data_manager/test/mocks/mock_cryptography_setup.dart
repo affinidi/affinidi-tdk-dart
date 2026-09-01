161a308b0c43acf0880c2e35e9e8c5c8a661fbd2
Marco Attanasio <marco.a@affinidi.com>
2026-09-01 16:24:06 +0200

    chore: revert change on mock to avoid bumping version on vault_data_manager package

--- packages/vault_data_manager/test/mocks/mock_cryptography_setup.dart
+++ packages/vault_data_manager/test/mocks/mock_cryptography_setup.dart
@@ -7,8 +7,8 @@ void setupMockCryptographyDefaults(MockCryptographyService mock) {
   when(() => mock.getRandomBytes(32)).thenReturn(mockBytes());
 
   when(
-    () => mock.pbkdf2FromBytes(
-      passwordBytes: any(named: 'passwordBytes'),
+    () => mock.Pbkdf2(
+      password: any(named: 'password'),
       nonce: any(named: 'nonce'),
     ),
   ).thenAnswer((_) async => mockBytes());
